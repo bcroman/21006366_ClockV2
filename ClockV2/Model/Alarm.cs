befb1bc4ee4afd8657144849572fe9758f019f4c
bcroman <bencollins315@gmail.com>
2025-04-22 15:27:42 +0100

    chore: Update Alarm File
    Update Alarm File with variables and methods to be used for priority queue

--- ClockV2/Model/Alarm.cs
+++ ClockV2/Model/Alarm.cs
@@ -8,5 +8,24 @@ namespace ClockV2.Model
 {
     public class Alarm
     {
+        //Variables
+        public DateTime Time { get; set; }
+        public string Label { get; set; }
+
+        public Alarm(DateTime time, string label = "")
+        {
+            Time = time;
+            Label = label;
+        }
+
+        public int CompareTo(Alarm other)
+        {
+            return Time.CompareTo(other.Time);
+        }
+
+        public override string ToString()
+        {
+            return $"{Label} - {Time:HH:mm:ss}";
+        }
     }
 }
