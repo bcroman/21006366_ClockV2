0bac2425b4188f25817360feb7ca01c06735b79f
bcroman <bencollins315@gmail.com>
2025-04-24 10:59:04 +0100

    chore: Alarm Manager
    Update Alarm Manager to have methods to use the priority queue functions and created a ref to the PriorityQueue Program

--- ClockV2/Model/AlarmManager.cs
+++ ClockV2/Model/AlarmManager.cs
@@ -1,12 +1,41 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PriorityQueue;
 
 namespace ClockV2.Model
 {
     public class AlarmManager
     {
+        private PriorityQueue<Alarm> alarmQueue;
+
+        public AlarmManager()
+        {
+            // Initialize with implementation of SortedArrayPriorityQueue
+            alarmQueue = new SortedArrayPriorityQueue<Alarm>(10);
+        }
+
+        public void AddAlarm(Alarm alarm, int priority)
+        {
+            alarmQueue.Add(alarm, priority);
+        }
+
+        public Alarm GetNextAlarm()
+        {
+            return alarmQueue.Head();
+        }
+
+        public void RemoveNextAlarm()
+        {
+            alarmQueue.Remove();
+        }
+
+        public bool IsQueueEmpty()
+        {
+            return alarmQueue.IsEmpty();
+        }
+
+        public override string ToString()
+        {
+            return alarmQueue.ToString();
+        }
     }
 }
